cb7d1de9989045a557e529d3c6cca6b735429272
markiz-karabas <vsevolodova@inbox.ru>
2024-01-06 20:16:31 +0300

    refactor: chenge naming of identifiers and classes according to rule 3.2 in html-and-css.md

--- app.js
+++ app.js
@@ -28,14 +28,14 @@ var createNewTaskElement=function(taskString) {
   var deleteButtonImg=document.createElement("img");//delete button image
   
   label.innerText=taskString;
-  label.className='task';
+  label.className='task-box__label';
   //Each elements, needs appending
   checkBox.type="checkbox";
   editInput.type="text";
-  editInput.className="task";
+  editInput.className="task-box__input";
   editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-  editButton.className="edit";
-  deleteButton.className="delete";
+  editButton.className="btn-edit";
+  deleteButton.className="btn-delete";
   deleteButtonImg.src='./remove.svg';
   deleteButton.appendChild(deleteButtonImg);
   //and appending.
@@ -65,7 +65,7 @@ var editTask=function() {
   var listItem=this.parentNode;
   var editInput=listItem.querySelector('input[type=text]');
   var label=listItem.querySelector("label");
-  var editBtn=listItem.querySelector(".edit");
+  var editBtn=listItem.querySelector(".btn-edit");
   var containsClass=listItem.classList.contains("edit-mode");
   //If class of the parent is .editmode
   if(containsClass){
@@ -124,8 +124,8 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
   console.log("bind list item events");
   //select ListItems children
   var checkBox=taskListItem.querySelector("input[type=checkbox]");
-  var editButton=taskListItem.querySelector("button.edit");
-  var deleteButton=taskListItem.querySelector("button.delete");
+  var editButton=taskListItem.querySelector(".btn-edit");
+  var deleteButton=taskListItem.querySelector(".btn-delete");
   
   //Bind editTask to edit button.
   editButton.onclick=editTask;
